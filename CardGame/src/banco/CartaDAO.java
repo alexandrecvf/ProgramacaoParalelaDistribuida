a13c16ed52a401cee7ee807c63685a4abdd5ab50
Alexandre Vilarinho <33914918+alexandrecvf@users.noreply.github.com>
2019-10-30 09:30:18 -0300

    Método Adicionar Pronto

---
+++ CardGame/src/banco/CartaDAO.java
@@ -0,0 +1,40 @@
+package banco;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+
+import javax.servlet.ServletException;
+
+import classes.Carta;
+
+public class CartaDAO {
+	public Connection conexao;
+	
+	public CartaDAO() throws ServletException {
+		this.conexao = new ConnectionFactory().getConnection();
+		System.out.println("Conectado!");
+	}
+	
+	public void adicionar(Carta carta) {
+		String sql = "insert into carta (nomeCarta, mana, imagem, tipoDaCarta, ataque, defesa, cartaDescricao) values (?,?,?,?,?,?,?)";
+		
+		try {
+			PreparedStatement ps = conexao.prepareStatement(sql);
+			
+			ps.setString(1, carta.getNomeCarta());
+			ps.setInt(2, carta.getMana());
+			ps.setString(3, carta.getImagem());
+			ps.setString(4, carta.getTipoDaCarta());
+			ps.setInt(5, carta.getAtaque());
+			ps.setInt(6, carta.getDefesa());
+			ps.setString(7, carta.getCartaDescricao());
+			
+			ps.execute();
+			ps.close();
+			
+		} catch (Exception e) {
+			// TODO: handle exception
+			System.out.println("Não foi adicionado!");
+		}
+	}
+}
--- CardGame/src/banco/ConnectionFactory.java
+++ CardGame/src/banco/ConnectionFactory.java
@@ -9,7 +9,7 @@ public class ConnectionFactory {
 	public Connection getConnection() throws ServletException{
 		try {
 			DriverManager.registerDriver(new com.mysql.jdbc.Driver());
-			return DriverManager.getConnection("jdbc:mysql://localhost/aula18", "root", "");
+			return DriverManager.getConnection("jdbc:mysql://localhost/carta_bd", "root", "");
 		} catch (Exception e) {
 			// TODO: handle exception
 			System.out.println("Não conectou");
